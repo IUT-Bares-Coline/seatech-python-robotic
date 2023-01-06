11b4bc0eebee6c68bb140cbccb4e24d78917ab57
IUT-Bares-Coline <coline.bares@gmail.com>
2023-01-06 10:00:15 +0100

    Version 2

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -16,49 +16,126 @@ Exoooooo 1
     Je jouerai alors avec votre objet pour interagir avec lui
 
  """
-
+import time
 
 class Robot():
     __name = "<unnamed>"
     __power = False
     __current_speed = 0
-    __battery_level = 0
+    __battery_level = 30
     __states = ['shutdown', 'running']
     
         
     """
       Give your best code here ( •̀ ω •́ )✧
     """
+    #__slots__ = ('name', 'power')
+	
+    def __init__(self, name='Jean_Charles', power=False):
+        self.__name = name
+        self.__power = power
 
-    def donner_nom(self, nom):
-        __name = nom
+    def donner_nom(self, nom = 'Jean_Charles'):
+        self.__name = nom
         pass
 
     def allumer(self):
         print('Bonjour ! < O_O >')
-        __power = True 
-        #__states =
+        self.__power = True 
+        self.__states = 'running'
         pass
 
     def eteindre(self):
         print('Au revoir ! < -_- >')
-        __power = False
-        #__states =
+        self.__power = False
+        self.__states = 'shutdown'
         pass
 
     def charger(self): 
-        for i in 10 :
-            __battery_level +=1
-            print('Niveau batterie : ', __battery_level)
-            #attendre 1s
+        Affichage_batt = ['-']
+        print('Niveau batterie : ')
+        batt_a_remplir = int(( 100 - self.__battery_level) / 10)
+
+        for i in range(batt_a_remplir) :
+            self.__battery_level +=10
+            Affichage_batt.append('-')
+            #print(self.__battery_level, '%')
+            print(Affichage_batt, self.__battery_level, '%')
+            time.sleep(1)
+
+        if(self.__battery_level!=100):
+            self.__battery_level = 100
+            print(self.__battery_level, '%')
+            time.sleep(1)
+        print('\n')
+        pass
+    
+    def deplacement(self, direction = 'droite', vitesse = 15):
+        if(self.__power == True & self.__battery_level > 0.5*vitesse):
+            if(direction == 'gauche'):
+                positionX -= vitesse
+            elif (direction == 'droite'):
+                positionX += vitesse
+            elif(direction == 'devant'):
+                positionY += vitesse
+            elif (direction == 'derriere'):
+                positionY -= vitesse
+            
+            __current_speed = vitesse
+            self.__battery_level -= 0.5*vitesse
         pass
 
-    def deplacement(self, direction):
-        if(direction == 'gauche'):
-            positionX -= 15
-        elif (direction == 'droite'):
-            positionX += 15
-        elif(direction == 'devant'):
-            positionY += 15
-        elif (direction == 'derriere'):
-            positionY -= 15
+    def vitesse_actuelle(self):
+        print('Vitesse actuelle : ', self.__current_speed) 
+        pass
+
+    def etat_global(self):
+        print('Nom du robot : ', self.__name)
+        print('Vitesse du robot : ', self.__current_speed)
+        print('Niveau de batterie du robot : ', self.__battery_level)
+        print('Etat du robot : ', self.__states)
+
+
+#MAIN
+
+	
+r = Robot()
+
+print('\n///////////Mise en marche du robot///////////\n')
+r.allumer()
+
+print('\n///////////Nommage du robot///////////\n')
+r.donner_nom('R2D2')
+
+
+print('\nAVANT CHARGEMENT ET CONSIGNE DE DEPLACEMENT\n')
+print('\n///////////Affichage de l état du robot///////////\n')
+r.etat_global()
+
+
+print('\nAVANT CHARGEMENT ET APRES CONSIGNE DE DEPLACEMENT\n')
+print('\n///////////Déplacement du robot (direction = gauche et vitesse = 8m/s (Mais en fait non parce que pas de batterie ! :/  )///////////\n')
+r.deplacement('gauche',8)
+
+print('\n///////////Affichage de la vitesse du robot///////////\n')
+r.vitesse_actuelle()
+
+print('\n///////////Affichage de l état du robot///////////\n')
+r.etat_global()
+
+
+print('\nAPRES CHARGEMENT ET CONSIGNE DE DEPLACEMENT\n')
+r.charger()
+print('\n///////////Déplacement du robot (direction = gauche et vitesse = 8m/s (et ouais il est rapide le loulou !))///////////\n')
+r.deplacement('gauche',8)
+
+print('\n///////////Affichage de la vitesse du robot///////////\n')
+r.vitesse_actuelle()
+
+print('\n///////////Affichage de l état du robot///////////\n')
+r.etat_global()
+
+print('\n///////////Mise à l arrêt du robot///////////\n')
+r.eteindre()
+
+print('\n///////////END OF LIFE///////////\n')
