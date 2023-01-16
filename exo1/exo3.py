2e58ee1c5d28cc7f7870aecf6702fb52e6728a62
IUT-Bares-Coline <coline.bares@gmail.com>
2023-01-16 10:49:46 +0100

    hh

--- exo1/exo3.py
+++ exo1/exo3.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+import time
 
 """ You can use classes below or create your own 👍️"""
 
@@ -9,7 +10,7 @@ class UnmannedVehicle(metaclass=ABCMeta):
     """
 
     @abstractmethod
-    def avancer(self):
+    def demarrer(self):
         pass
 
     @abstractmethod
@@ -45,15 +46,15 @@ class GroundVehicle(metaclass=ABCMeta):
     """ A vehicle made for ground fields."""
 
     @abstractmethod
-    def Demarrer(self):
+    def demarrer2(self):
         pass
 
     @abstractmethod
-    def Rouler(self):
+    def rouler(self):
         pass
 
     @abstractmethod
-    def Arreter(self):
+    def arreter(self):
         pass
 
     pass
@@ -79,65 +80,157 @@ class UnderseaVehicle(metaclass=ABCMeta):
 
 class UAV(AerialVehicle, UnmannedVehicle):
     """Unmanned Aerial Vehicle"""
-    def avancer(self):
-        print('L UAV avance')
+    altitude = 0
+    destination = 'La Réunion'
+
+    def demarrer(self):
+        print("Mise en marche des moteurs de l'UAV.")
 
     def mission(self):
-        print('L UAV effectue sa mission automatiquement')    
-    
+        print("L'UAV effectue sa mission automatiquement jusqu'à ", self.destination)  
+   
     def config_mission(self):
-        print('Configuration de la mission de l UAV')
+        self.destination = input("Configuration de la mission de l'UAV : \n   Entrer une destination : ")
 
     def decoller(self):
-        print('Decollage de l UAV')
+        self.altitude = 10
+        print("Decollage de l'UAV")
 
     def voler(self):
-        print('Vol en cours de l UAV')
+        if(self.altitude != 10):
+            print("Il faut d'abord décoller avant de voler !!!")
+        else :
+            print("Vol en cours de l'UAV")
+            print('----', end='')
+            time.sleep(1)
+            print('----')
+            time.sleep(1)
+            print('       |')
+            time.sleep(1)
+            print('       |')
+            time.sleep(1)
+            print('       ----')
+            time.sleep(1)
+            print('           |')
+            time.sleep(1)
+            print('            ----[o_o]  |> ', self.destination)
+            time.sleep(1)
 
     def atterir(self):
-        print('Atterissage du l UAV')
+        self.altitude = 0
+        print("Atterissage du l'UAV")
 
     pass
 
 
 class UUV(UnderseaVehicle, UnmannedVehicle):
     """Unmanned Undersea Vehicle"""
+    altitude = 0
+    destination = 'La Réunion'
 
-    def avancer(self):
-        print('L UUV avance')
+    def demarrer(self):
+        print("Mise en marche des moteurs de l'UUV.")
 
     def mission(self):
-        print('L UUV effectue sa mission automatiquement')    
+        print("L'UUV effectue sa mission automatiquement jusqu'à ", self.destination)    
     
     def config_mission(self):
-        print('Configuration de la mission de l UUV')
+        self.destination = input("Configuration de la mission de l'UUV : \n   Entrer une destination : ")
 
-    pass
+    def plonger(self):
+        self.altitude = -10
+        print("Plongeon de l'UUV")
+
+    def nager(self):
+        if(self.altitude != -10):
+            print("Il faut d'abord plonger avant de nager !!!")
+        else :
+            print('L UUV nage')
+            print('----', end='')
+            time.sleep(1)
+            print('----')
+            time.sleep(1)
+            print('       |')
+            time.sleep(1)
+            print('       |')
+            time.sleep(1)
+            print('       ----')
+            time.sleep(1)
+            print('           |')
+            time.sleep(1)
+            print('            ----[o_o]  |> ', self.destination)
+            time.sleep(1)
+
+    def sortir_eau(self):
+        print('Sortie de l eau de l UUV')
 
 
 class UGV(GroundVehicle, UnmannedVehicle):
     """Unmanned Ground Vehicle"""
 
-    def avancer(self):
-        print('L UGV avance')
+    def demarrer(self):
+        print("Mise en marche des moteurs de l'UGV.")
 
     def mission(self):
-        print('L UGV effectue sa mission automatiquement')    
+        print("L'UGV effectue sa mission automatiquement")    
     
     def config_mission(self):
-        print('Configuration de la mission de l UUGV')
+        print('Configuration de la mission de l UGV')
+
+    def demarrer2(self):
+        print('Demarrage de l UGV')
+
+    def rouler(self):
+        print('L UGV roule')
+
+    def arreter(self):
+        print('Arrêt de l UGV')
 
     pass
 
 
-uav = UAV(UnmannedVehicle)
-uav.do_something_interesting()
-uav.do_something_aerial_specific()
 
-ugv = UGV()
-ugv.do_something_interesting()
-ugv.do_something_ground_specific()
+if __name__ == "__main__" :
+    
+    print('///////CREATION DE L UAV/////// \n')
+    """
+    print ('...', end='')
+    time.sleep(1)
+    print('CRE', end='')
+    time.sleep(1)
+    print('ATION', end='')
+    time.sleep(1)
+    print(' DE ', end='')
+    time.sleep(1)
+    print("L'UAV", end='')
+    time.sleep(1)
+    print('...')
+    """
 
-uuv = UUV()
-uuv.do_something_interesting()
-uuv.do_something_undersea_specific()
+    uav = UAV()
+    uav.demarrer()
+    uav.config_mission()
+    uav.mission()
+    uav.decoller()
+    uav.voler()
+    uav.atterir()
+
+    print('\n\n///////CREATION DE L UUV/////// \n')
+    uuv = UUV()
+    uuv.demarrer()
+    uuv.config_mission()
+    uuv.mission()
+    uuv.plonger()
+    uuv.nager()
+    uuv.sortir_eau()
+
+    print('\n\n///////CREATION DE L UGV/////// \n')
+    ugv = UGV()
+    ugv.demarrer()
+    ugv.config_mission()
+    ugv.mission()
+    ugv.demarrer()
+    ugv.rouler()
+    ugv.arreter()
+
+
